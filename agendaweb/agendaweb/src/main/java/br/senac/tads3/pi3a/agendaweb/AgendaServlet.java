fddec74e0d492bcb11afc5d0e98ac2ff4d10cbd0
Fernando Tsuda <ftsuda.senac@gmail.com>
2017-03-31 20:54:39 -0300

    Servlet - primeiro exemplo

---
+++ agendaweb/agendaweb/src/main/java/br/senac/tads3/pi3a/agendaweb/AgendaServlet.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads3.pi3a.agendaweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+@WebServlet({"/agenda"})
+public class AgendaServlet extends HttpServlet {
+  
+  @Override
+  public void doGet(HttpServletRequest request, 
+	  HttpServletResponse response) 
+	  throws ServletException {
+    
+    response.setContentType("text/plain");
+    try (PrintWriter out = response.getWriter()) {
+      out.print("Olá mundo Servlet Web");
+      out.flush();
+    } catch (IOException ex) {
+      
+    }
+  }
+  
+}
